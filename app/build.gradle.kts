71a9ca8f312cabc9ad98c80bbd7d9ccec56ae2b9
khondokermoin <mouin2k19@gmail.com>
2024-07-03 18:39:42 +0600

    FireStore complite

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,11 +54,11 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.2")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.2")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.3")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.3")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.3")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.3")
 
     implementation("androidx.fragment:fragment-ktx:1.8.1")
     debugImplementation("androidx.fragment:fragment-testing:1.8.1")
--- app/src/main/java/com/example/locationsharing/FireStoreViewModel.kt
+++ app/src/main/java/com/example/locationsharing/FireStoreViewModel.kt
@@ -1,4 +1,79 @@
 package com.example.locationsharing
 
-class FireStoreViewModel {
+
+import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.FirebaseFirestore
+
+class FireStoreViewModel: ViewModel() {
+    private val firestore = FirebaseFirestore.getInstance()
+    private val userCollection = firestore.collection("users")
+
+    fun saveUser(userId:String, displayName:String, email:String, location:String){
+        val user = hashMapOf(
+            "displayName" to displayName,
+            "email" to email,
+            "location" to location
+        )
+        userCollection.document(userId).set(user)
+            .addOnSuccessListener {  }
+            .addOnFailureListener { e-> }
+    }
+    fun getAllUser(callback: (List<User>)->Unit){
+        userCollection.get()
+            .addOnSuccessListener{result->
+                val userList = mutableListOf<User>()
+                for(document in result){
+                    val userId = document.id
+                    val displayName = document.getString("displayName")?: ""
+                    val email = document.getString("email")?: ""
+                    val location = document.getString("location")?: ""
+
+                    userList.add(User(userId, displayName, email, location))
+                }
+                callback(userList)
+            }
+            .addOnFailureListener{}
+    }
+    fun updateUser(userId: String, displayName: String, email: String, location: String){
+        val user = hashMapOf(
+            "displayName" to displayName,
+            "location" to location
+        )
+        val userMap = user.toMap()
+        userCollection.document(userId).set(userMap)
+            .addOnSuccessListener {  }
+            .addOnFailureListener {  }
+    }
+    fun updateUserLocation(userId: String, location: String){
+        if (userId.isEmpty()){
+            return
+        }
+        val user = hashMapOf(
+            "location" to location
+        )
+        val userMap = user.toMap()
+        userCollection.document(userId).update(userMap)
+            .addOnSuccessListener {  }
+            .addOnFailureListener{}
+    }
+    fun getUser(userId: String, callback:(User?) -> Unit){
+        userCollection.document(userId).get()
+            .addOnSuccessListener { documentSnapshot ->
+                val user = documentSnapshot.toObject(User::class.java)
+                callback(user)
+            }
+            .addOnFailureListener{
+                callback(null)
+            }
+    }
+    fun getUserLocation(userId: String, callback: (String) -> Unit){
+        userCollection.document(userId).get()
+            .addOnSuccessListener { documentSnapShort->
+                val location = documentSnapShort.getString("location")?:""
+                callback(location)
+            }
+            .addOnFailureListener{
+                callback("")
+            }
+    }
 }
