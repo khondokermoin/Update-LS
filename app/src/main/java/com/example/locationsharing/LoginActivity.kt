c3a47e2b6ac9771d0ac9a4c4321e438cd1ccf454
khondokermoin <mouin2k19@gmail.com>
2024-07-03 20:54:26 +0600

    LoginActivity complite

--- app/src/main/java/com/example/locationsharing/LoginActivity.kt
+++ app/src/main/java/com/example/locationsharing/LoginActivity.kt
@@ -1,15 +1,57 @@
 package com.example.locationsharing
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var viewModel: AuthenticationViewModel
+    private lateinit var buttonLogin: Button
+    private lateinit var editTextEmail:EditText
+    private lateinit var editTextPassword: EditText
+    private lateinit var textViewRegister: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        buttonLogin = findViewById(R.id.loginBtn)
+        editTextEmail = findViewById(R.id.emailEt)
+        editTextPassword = findViewById(R.id.passwordEt)
+        textViewRegister = findViewById(R.id.registerTxt)
+
+        viewModel = ViewModelProvider(this).get(AuthenticationViewModel::class.java)
+
+        buttonLogin.setOnClickListener {
+            val email = editTextEmail.text.toString()
+            val password = editTextPassword.text.toString()
+
+            viewModel.login(email, password, {
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }, { errorMessage ->
+                Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
+            })
+        }
+        textViewRegister.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (Firebase.auth.currentUser!=null) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/locationsharing/RegisterActivity.kt
+++ app/src/main/java/com/example/locationsharing/RegisterActivity.kt
@@ -1,15 +1,66 @@
 package com.example.locationsharing
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var authViewModel: AuthenticationViewModel
+    private lateinit var fireStoreViewModel: FireStoreViewModel
+    private lateinit var buttonRegister: Button
+    private lateinit var editTextEmail: EditText
+    private lateinit var editTextPassword: EditText
+    private lateinit var editTextDisplayName: EditText
+    private lateinit var textViewLogin: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
+        buttonRegister = findViewById(R.id.registerBtn)
+        editTextEmail = findViewById(R.id.emailEt)
+        editTextPassword = findViewById(R.id.passwordEt)
+        editTextDisplayName = findViewById(R.id.displayNameEt)
+        textViewLogin = findViewById(R.id.loginTxt)
+
+        authViewModel = ViewModelProvider(this).get(AuthenticationViewModel::class.java)
+        fireStoreViewModel = ViewModelProvider(this).get(FireStoreViewModel::class.java)
+
+        buttonRegister.setOnClickListener {
+            val email = editTextEmail.text.toString()
+            val password = editTextPassword.text.toString()
+
+            authViewModel.register(email, password, {
+                val displayName = editTextDisplayName.text.toString()
+                val location = "Location not available"
+
+                fireStoreViewModel.saveUser(authViewModel.getCurrentUserId(), displayName, email, location)
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }, { errorMessage ->
+                Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
+            })
+        }
+
+        textViewLogin.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+    }
+    override fun onStart() {
+        super.onStart()
+        if(Firebase.auth.currentUser!=null){
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
     }
 }
